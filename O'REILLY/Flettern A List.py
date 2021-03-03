c5ff03ec3017794004ee64985cc9d59d56690329
Omen4Dead <69041761+Omen4Dead@users.noreply.github.com>
2021-03-03 21:24:10 +0500

    111

---
+++ O'REILLY/Flettern A List.py
@@ -0,0 +1,20 @@
+def flat_list(array):
+    lst = []
+
+    def func(ls):
+        for i in ls:
+            if type(i) is list:
+                func(i)
+            else:
+                lst.append(i)
+        return lst
+    res = func(array)
+    return res
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
